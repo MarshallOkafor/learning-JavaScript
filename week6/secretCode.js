a856bc64ccd3e60f60fd02afd833cab40a391cc9
Marshall Okafor <marshallokafor@gmail.com>
2023-03-06 21:08:23 -0600

    Added practice codes for week 6

---
+++ week6/secretCode.js
@@ -0,0 +1,7 @@
+// JavaScript code that interacts with the 
+// secret code web page using the DOM
+var code = document.getElementById("code3");
+var secret = code.innerHTML;
+
+// Display the secret code
+alert(secret);
